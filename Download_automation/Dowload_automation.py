c6c8f0e9be1a29c6c7f2247b74c72ae6253e56de
sam-jam-sam <samuelscottmiller@gmail.com>
2024-02-16 14:21:54 +1100

    clean up

--- Download_automation/Dowload_automation.py
+++ Download_automation/Dowload_automation.py
@@ -1,21 +1,18 @@
-import os
+from os import scandir
 
 from shutil import move
 
 source_dir = 'C:/Users/samue/Downloads'
 dest_model_dir = 'C:/Users/samue/OneDrive/3D Models'
 
-model_extensons = (".stl", ".3mf")
+model_extensons = (".stl", ".3mf", ".STL", "3MF")
 
 def move_models():
-    with os.scandir(source_dir) as entries:
+    with scandir(source_dir) as entries:
         for entry in entries:
             name = entry.name
             if name.endswith(model_extensons):
                 move(entry, dest_model_dir)
                 print(name)
-            
-            #if name.endswith(model_extensons) or name.endswith(model_extensons.upper()):
-            #    print(entry)
 
 move_models()
